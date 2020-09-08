fc04fed7d8c2db9725b1b8ab09cc3aee73fb36ae
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-09-08 10:22:57 +0200

    Work with DBs

--- app/pages/posts/[slug].tsx
+++ app/pages/posts/[slug].tsx
@@ -1,18 +1,20 @@
 import { useParams, useQuery } from "blitz"
 import getPost from "../../posts/queries/getPost"
+import { Suspense } from "react"
 
-const PostPage = () => {
-  try {
-    const [post] = useQuery(getPost, { where: { id: 1 } })
-    return (
-      <div>
-        <h1>Post:{post.title}</h1>
-      </div>
-    )
-  } catch (e) {
-    console.log(e)
-  }
-  return null
+const Page = () => {
+  const [post] = useQuery(getPost, { where: { id: 1 } })
+  return (
+    <div>
+      <h1>Post:{post.title}</h1>
+    </div>
+  )
 }
+
+const PostPage = () => (
+  <Suspense fallback={<span>Loading...</span>}>
+    <Page />
+  </Suspense>
+)
 
 export default PostPage
